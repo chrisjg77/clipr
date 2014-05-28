a2179ebbed40a51086f29a3a266f33f4aae001cb
chrisjg77 <chrisg@terraeclipse.com>
2014-05-27 17:27:34 -0700

    Adds HTML5 video functions to app

--- public/js/plugins/range-slider.js
+++ public/js/plugins/range-slider.js
@@ -1,5 +1,6 @@
 define(function (require) {
   var app = require('app')
+    , $video = $('.video')[0]
     ;
 
   // Vendor plugins.
@@ -9,14 +10,59 @@ define(function (require) {
   app.rangeSlider = {};
 
   app.rangeSlider.init = function() {
+
+    $('.video')[0].autoplay = true;
+    $('.video')[0].muted = true;
+
+    var start_time = 0;
+
+    var looper = setInterval(function() {
+      $('.video')[0].currentTime = start_time;
+      $('.video')[0].play();
+    },10000);
+
     $('.slider-handle').draggable({
       axis: 'x',
       containment: ".slider",
+      drag: function() {
+        var slider_w = $('.slider').width(),
+            vid_time = $('.video')[0].duration,
+            px_to_min = vid_time/slider_w,
+            pos = $(this).position().left,
+            end_time = start_time+10;
+
+        $('.video')[0].currentTime = start_time;
+        $('.video')[0].pause();
+      },
       stop: function() {
-        var pos = $(this).position().left;
-        alert('stopped dragging at ' + pos + 'px');
+        var slider_w = $('.slider').width(),
+            vid_time = $('.video')[0].duration,
+            px_to_min = vid_time/slider_w,
+            pos = $(this).position().left,
+            end_time = start_time+10;
+
+        start_time = pos*px_to_min;
+
+        $('.video')[0].currentTime = start_time;
+        $('.video')[0].play();
+
+        // console.log(vid_time);
+        // console.log('Stopped dragging at '+pos+'px or '+timify(start_time)+'. Will stop at '+timify(end_time));
       }
     });
   };
+
+  function timify(time) {
+    var minutes = Math.floor(time / 60),
+        seconds = (Math.round(time - minutes*60)).toString();
+
+    if (seconds.length <= 1) {
+      console.log(seconds);
+      seconds = '0'+seconds;
+    }
+
+    return minutes+':'+seconds;
+  };
+
 
 });
